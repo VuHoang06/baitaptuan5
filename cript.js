1b61c63972dfa2a444094ab832a72e1c7080fd4d
VuHoang06 <24100381@st.phenikaa-uni.edu.vn>
2025-10-22 20:33:22 +0700

    Update cript.js

--- cript.js
+++ cript.js
@@ -1,19 +1,14 @@
-// Chọn div preview
 const preview = document.getElementById('preview');
 
-// Chọn tất cả ảnh thumbnail
 const thumbnails = document.querySelectorAll('.thumbnail');
 
-// Lặp qua từng ảnh
 thumbnails.forEach(img => {
     img.addEventListener('mouseover', function() {
-        // Khi di chuột vào, thay đổi background của div preview
         preview.style.backgroundImage = `url('${img.src}')`;
-        preview.textContent = img.alt; // Hiển thị alt
+        preview.textContent = img.alt; 
     });
 
     img.addEventListener('mouseout', function() {
-        // Khi rời chuột, trả về trạng thái ban đầu
         preview.style.backgroundImage = '';
         preview.textContent = 'Di chuột qua một hình ảnh bên dưới để hiển thị ở đây.';
     });
